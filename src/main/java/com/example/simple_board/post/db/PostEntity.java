7630b0dc690fc8d3537ff1106af3025b3313a7dd
정승민 <67104714+Codemaniac7@users.noreply.github.com>
2024-09-26 01:00:44 +0900

    simpleBoard

---
+++ src/main/java/com/example/simple_board/post/db/PostEntity.java
@@ -0,0 +1,44 @@
+package com.example.simple_board.post.db;
+
+
+import com.example.simple_board.reply.db.ReplyEntity;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Entity(name="post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long boardId;
+
+    private String userName;
+
+    private String password;
+
+    private String email;
+
+    private String status;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime postedAt;
+
+    @Transient
+    private List<ReplyEntity> replyList = List.of();
+}
